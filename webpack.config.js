2a116168d836e6e008c20c8d6b6c07a5e193e888
Kristoffer Svensson <ks222rt@student.lnu.se>
2017-04-19 16:58:33 +0200

    upgraded to webpack 2 and got everything to work. Got the less file required at last

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
     ExtractTextPlugin = require('extract-text-webpack-plugin');
+    path = require('path');
 
 module.exports = {
     context: __dirname + '/app',
@@ -9,42 +10,51 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        root: __dirname,
+        modules: [
+            path.resolve(__dirname, 'app'),
+            'node_modules'
+        ],
         alias: {
-            Nav: 'app/components/nav_component.jsx',
-            UtilBox: 'app/components/util_bar_component.jsx',
-            Main: 'app/components/main_component.jsx',
-            About: 'app/components/about_component.jsx',
-            Csgo: 'app/components/csgo_component.jsx'
+            Nav$: path.resolve(__dirname, 'app/components/nav_component.jsx'),
+            UtilBox$: path.resolve(__dirname, 'app/components/util_bar_component.jsx'),
+            Main$:  path.resolve(__dirname, 'app/components/main_component.jsx'),
+            About$:  path.resolve(__dirname, 'app/components/about_component.jsx'),
+            Csgo$:  path.resolve(__dirname, 'app/components/csgo_component.jsx')
         },
-        extensions: ['', '.js', '.jsx', '.css', '.less']
+        extensions: ['.js', '.jsx', '.css', '.less']
     },
     module: {
-        preLoaders: [
-            { loader: 'source-map' }
-        ],
-        loaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-0']
-                },              
+                use:[
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['react', 'es2015', 'stage-0']
+                        },
+                    }
+                ],              
                 exclude: /node_modules/  
             },            
             { 
                 test:  /\.(less|css)$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: "css-loader!less-loader"
-                }), 
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
             }
         ]
     },
     plugins: [
         new webpack.IgnorePlugin(new RegExp("^(fs|ipc)$")),
 
-        new ExtractTextPlugin('style.css')
+        new ExtractTextPlugin({
+            filename: 'style.css',
+            disable: false,
+            allChunks: true
+        })
     ],
 
     stats: { colors: true }
